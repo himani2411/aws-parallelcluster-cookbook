e1aa3c0bf0406847cdd5f617fbc909e61713148f
Himani Deshpande <himanidp@amazon.com>
2024-03-04 20:56:47 -0500

    Unit Test

--- cookbooks/aws-parallelcluster-platform/spec/unit/resources/fetch_config_spec.rb
+++ cookbooks/aws-parallelcluster-platform/spec/unit/resources/fetch_config_spec.rb
@@ -62,7 +62,83 @@ describe 'fetch_config:run' do
       is_expected.not_to run_execute("Wait cluster config files to be updated by the head node")
     end
   end
+  
+  context "when running a HeadNode from kitchen on update" do
+      cached(:cluster_shared_dir) { '/cluster_shared_dir' }
+      cached(:cluster_shared_dir_login_nodes) { '/shared_dir_login_nodes' }
+      cached(:cluster_config_path) { 'cluster_config_path' }
+      cached(:login_cluster_config_path) { 'login_cluster_config_path' }
+      cached(:previous_cluster_config_path) { 'cluster_config_path' }
+      cached(:cluster_config_version) { 'cluster_config_version' }
+      cached(:cluster_shared_storages_mapping_path) { '/cluster_shared_storages_mapping_path' }
+      cached(:cluster_previous_shared_storages_mapping_path) { '/cluster_previous_shared_storages_mapping_path' }
+      cached(:instance_types_data_path) { 'instance_types_data_path' }
+      cached(:previous_instance_types_data_path) { 'previous_instance_types_data_path' }
+      cached(:change_set_path) { 'change_set_path' }
+      cached(:shared_login_nodes_keys_sync_file_path) { '/shared_dir_login_nodes/.login_nodes_keys_sync_file' }
+      cached(:chef_run) do
+        runner = ChefSpec::Runner.new(
+          platform: 'ubuntu', step_into: %w(fetch_config)
+        ) do |node|
+          node.override['kitchen'] = true
+          node.override['cluster']['shared_dir'] = cluster_shared_dir
+          node.override['cluster']['cluster_config_path'] = cluster_config_path
+          node.override['cluster']['cluster_config_version'] = cluster_config_version
+          node.override['cluster']['previous_cluster_config_path'] = previous_cluster_config_path
+          node.override['cluster']['instance_types_data_path'] = instance_types_data_path
+          node.override['cluster']['previous_instance_types_data_path'] = previous_instance_types_data_path
+          node.override['cluster']['change_set_path'] = change_set_path
+          node.override['cluster']['shared_storages_mapping_path'] = cluster_shared_storages_mapping_path
+          node.override['cluster']['previous_shared_storages_mapping_path'] = cluster_previous_shared_storages_mapping_path
+          node.override['cluster']['login_cluster_config_path'] = login_cluster_config_path
+          node.override['cluster']['shared_dir_login_nodes'] = cluster_shared_dir_login_nodes
+          node.override['cluster']['shared_login_nodes_keys_sync_file'] = shared_login_nodes_keys_sync_file_path
+          node.override['cluster']['node_type'] = 'HeadNode'
+        end
+        allow(File).to receive(:exist?).with(cluster_config_path).and_return(true)
+        allow(FileUtils).to receive(:cp_r).with(
+          cluster_config_path, previous_cluster_config_path, remove_destination: true
+        ).and_return(true)
+        allow(File).to receive(:read).with(cluster_config_path).and_return('FAKE_CONFIG_CONTENT')
+        allow(FileUtils).to receive(:identical?).with(
+          cluster_config_path, previous_cluster_config_path
+        ).and_return(true)
+        allow(FileUtils).to receive(:cp_r).with(
+          instance_types_data_path, previous_instance_types_data_path, remove_destination: true
+        ).and_return(true)
+        allow(File).to receive(:read).with(change_set_path).and_return('FAKE_CHANGE_SET_CONTENT')
+        allow(FileUtils).to receive(:cp_r).with(
+          cluster_shared_storages_mapping_path, cluster_previous_shared_storages_mapping_path, remove_destination: true
+        ).and_return(true)
+        runner.converge_dsl do
+          fetch_config 'run' do
+            action :run
+            update true
+          end
+        end
+      end
 
+      # it "copies data from kitchen data dir" do
+      #   is_expected.to create_remote_file("copy fake cluster config")
+      #                    .with(path: cluster_config_path)
+      #                    .with(source: "file://#{kitchen_cluster_config_path}")
+      #
+      #   is_expected.to create_remote_file("copy fake instance type data")
+      #                    .with(path: instance_types_data_path)
+      #                    .with(source: "file://#{kitchen_instance_types_data_path}")
+      # end
+
+      it "writes the synchronization file for login nodes" do
+        is_expected.to create_file(shared_login_nodes_keys_sync_file_path).with(
+          content: config_version_file,
+          mode: '0644',
+          owner: 'root',
+          group: 'root'
+        )
+      end
+      
+  end
+  
   %w(ComputeFleet LoginNode).each do |node_type|
     context "when running a #{node_type} from kitchen on create" do
       cached(:cluster_config_path) { 'cluster_config_path' }
@@ -111,6 +187,7 @@ describe 'fetch_config:run' do
       cached(:cluster_previous_shared_storages_mapping_path) { '/cluster_previous_shared_storages_mapping_path' }
       cached(:instance_types_data_path) { 'instance_types_data_path' }
       cached(:previous_instance_types_data_path) { 'previous_instance_types_data_path' }
+      cached(:shared_login_nodes_keys_sync_file_path) { '/shared_dir_login_nodes/.login_nodes_keys_sync_file' }
       cached(:chef_run) do
         runner = ChefSpec::Runner.new(
           platform: 'ubuntu', step_into: %w(fetch_config)

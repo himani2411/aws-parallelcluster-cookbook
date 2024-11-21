f9abdb155847f9d66dc560b0288b4aea82cbd9ab
Himani Deshpande <himanidp@amazon.com>
2024-11-21 15:03:12 -0500

    Upgrade IntelMPI to 2021.14.0

--- cookbooks/aws-parallelcluster-platform/recipes/install/intel_mpi.rb
+++ cookbooks/aws-parallelcluster-platform/recipes/install/intel_mpi.rb
@@ -16,7 +16,7 @@
 # limitations under the License.
 
 intelmpi_supported = !arm_instance?
-intelmpi_version = '2021.13'
+intelmpi_version = '2021.14'
 
 node.default['conditions']['intel_mpi_supported'] = intelmpi_supported
 node.default['cluster']['intelmpi']['version'] = intelmpi_version
@@ -25,11 +25,11 @@ node_attributes "dump node attributes"
 
 return unless intelmpi_supported
 
-intelmpi_full_version = "#{intelmpi_version}.1.769"
+intelmpi_full_version = "#{intelmpi_version}.0.791"
 intelmpi_installation_path = "/opt/intel/mpi/#{intelmpi_version}"
-intelmpi_installer = "l_mpi_oneapi_p_#{intelmpi_full_version}_offline.sh"
+intelmpi_installer = "intel-mpi-#{intelmpi_full_version}_offline.sh"
 intelmpi_installer_path = "#{node['cluster']['sources_dir']}/#{intelmpi_installer}"
-intelmpi_installer_url = "#{node['cluster']['artifacts_s3_url']}/impi/#{intelmpi_installer}"
+intelmpi_installer_url = "https://registrationcenter-download.intel.com/akdlm/IRC_NAS/4b14b28c-2ca6-4559-a0ca-8a157627e0c8/#{intelmpi_installer}"
 intelmpi_qt_version = '6.5.3'
 
 # Prerequisite for module install

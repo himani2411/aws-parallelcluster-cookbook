ad9773d9aeaa712dd25c70e700bcd9c42c0690b9
Himani Anil Deshpande <himanidp@amazon.com>
2026-01-28 13:12:58 -0500

    [MYSQL] Upgrade MySQL from 8.0.39 to 8.4.8

--- cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
+++ cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
@@ -16,3 +16,7 @@ if platform?('amazon') && node['platform_version'] == "2"
   default['cluster']['jwt']['sha256'] = '617778f9687682220abf9b7daacbe72bab7c2985479f8bee4db9648bd2440687'
 end
 default['cluster']['jwt']['base_url'] = "#{node['cluster']['artifacts_s3_url']}/dependencies/jwt"
+## MySql
+default['cluster']['mysql']['version'] = "#{node['cluster']['mysql']['version']}-1"
+default['cluster']['mysql']['source_version'] = '8.4.8'
+default['cluster']['mysql']['base_url'] = "#{node['cluster']['artifacts_s3_url']}/mysql"

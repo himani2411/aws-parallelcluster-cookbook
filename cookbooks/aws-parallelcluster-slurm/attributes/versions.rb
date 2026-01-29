899a272954970a7c3ee1077410cc82e611c8065e
Himani Anil Deshpande <himanidp@amazon.com>
2026-01-29 16:44:47 -0500

    [Munge] Update Munge for all OSS except AL2

--- cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
+++ cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
@@ -7,6 +7,10 @@ default['cluster']['slurm']['base_url'] = "#{node['cluster']['artifacts_s3_url']
 # Munge
 default['cluster']['munge']['munge_version'] = '0.5.17'
 default['cluster']['munge']['sha256'] = '4d6a1b9665d8a1119fb90678e6bcf446012340dc59dbcc90a10e2ab2e4724f08'
+if platform?('amazon') && node['platform_version'] == "2"
+  default['cluster']['munge']['munge_version'] = '0.5.16'
+  default['cluster']['munge']['sha256'] = 'fa27205d6d29ce015b0d967df8f3421067d7058878e75d0d5ec3d91f4d32bb57'
+end
 default['cluster']['munge']['base_url'] = "#{node['cluster']['artifacts_s3_url']}/dependencies/munge"
 # LibJwt
 default['cluster']['jwt']['version'] = '1.18.4'

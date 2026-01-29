268b060ebd167f7be1444017018a7f892b636141
Himani Anil Deshpande <himanidp@amazon.com>
2026-01-29 12:42:43 -0500

    [Mysql] Upgrade MYSql for all OS except AL2

--- cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
+++ cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
@@ -17,6 +17,10 @@ if platform?('amazon') && node['platform_version'] == "2"
 end
 default['cluster']['jwt']['base_url'] = "#{node['cluster']['artifacts_s3_url']}/dependencies/jwt"
 ## MySql
-default['cluster']['mysql']['version'] = "#{node['cluster']['mysql']['version']}-1"
+
 default['cluster']['mysql']['source_version'] = '8.4.8'
+if platform?('amazon') && node['platform_version'] == "2"
+  default['cluster']['mysql']['source_version'] = '8.0.39'
+end
+default['cluster']['mysql']['version'] = "#{node['cluster']['mysql']['version']}-1"
 default['cluster']['mysql']['base_url'] = "#{node['cluster']['artifacts_s3_url']}/mysql"

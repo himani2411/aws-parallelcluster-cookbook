8fcbbaa30b6b18d27a07b080f9dde9917ce72a8d
Himani Anil Deshpande <himanidp@amazon.com>
2026-01-30 10:53:44 -0500

    [Mysql Update spec to use mysql version

--- cookbooks/aws-parallelcluster-slurm/test/controls/mysql_client_spec.rb
+++ cookbooks/aws-parallelcluster-slurm/test/controls/mysql_client_spec.rb
@@ -31,7 +31,7 @@ control 'tag:install_mysql_client_installed' do
   mysql_packages.each do |pkg|
     describe package(pkg) do
       it { should be_installed }
-      its('version') { should match /^8.0.39-/ } unless ubuntu
+      its('version') { should match /^#{node['cluster']['mysql']['source_version']}-/ } unless ubuntu
     end
   end
 end
